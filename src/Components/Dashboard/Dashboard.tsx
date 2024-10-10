40a0cafcb7f95f7c9686262b7ff017dcc196f0de
ranagithubrr <ranarr.dev@gmail.com>
2024-10-11 00:06:21 +0600

    Feat: React helmet added

--- src/Components/Dashboard/Dashboard.tsx
+++ src/Components/Dashboard/Dashboard.tsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import { Helmet } from "react-helmet";
 
 const Dashboard = () => {
   return (
-    <div>Main dashboard
+    <div>
+      <Helmet>
+        <title>nexaDemy | Dashboard </title>
+      </Helmet>
 
 
       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem laborum iste quisquam velit, enim natus nostrum voluptate maiores quae! Suscipit animi, tempora unde odit a nulla. Alias saepe temporibus consequuntur.</p>
