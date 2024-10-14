32f3af2a22e0088ff9fbef8b148d64c6c58fd1a2
ranagithubrr <ranarr.dev@gmail.com>
2024-10-14 11:56:34 +0600

    merged oauth into development

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { BrowserRouter as Router } from "react-router-dom";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import App from "./App";
+import './index.css'
 
-createRoot(document.getElementById("root")).render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
   <StrictMode>
     <Router>
       <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
